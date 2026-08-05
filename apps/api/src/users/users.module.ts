c34cabdc85f774907f3d3efe9f9d77a2ca000998
laszlo <proglaszlo@gmail.com>
2026-08-05 20:45:11 +0200

    merge előtti kommit

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { MailTokenModule } from '../mail-token/mail-token.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+    imports: [
+    PrismaModule,
+    MailTokenModule,
+    JwtModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
    exports: [UsersService],
